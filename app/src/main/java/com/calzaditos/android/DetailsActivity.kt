1eb237b090f35ed8f9f2e906e9a5c01012fe8d99
ElizSanchez <eliz1626@gmail.com>
2025-06-24 13:13:01 -0500

    boton añadir

--- app/src/main/java/com/calzaditos/android/DetailsActivity.kt
+++ app/src/main/java/com/calzaditos/android/DetailsActivity.kt
@@ -3,7 +3,6 @@ package com.calzaditos.android
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -49,10 +48,13 @@ class DetailsActivity : AppCompatActivity() {
 
     }
 
+
     fun returnProduct(view: View) {
         val intent = Intent(this, ProductsActivity::class.java)
         startActivity(intent)
     }
+
+
 
 
 }
