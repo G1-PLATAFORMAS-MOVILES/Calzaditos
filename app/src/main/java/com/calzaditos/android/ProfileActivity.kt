8d768eb3d98480cdc2fd3f9e9ee7c9ecf7bce142
Eliz Sanchez <U202316462@upc.edu.pe>
2025-07-17 12:42:14 -0500

    Se agregó nombre

--- app/src/main/java/com/calzaditos/android/ProfileActivity.kt
+++ app/src/main/java/com/calzaditos/android/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.calzaditos.android
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,11 +20,17 @@ class ProfileActivity : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val loggedUserName = getSharedPreferences("calzaditos", MODE_PRIVATE).getString("userName", "")
+        findViewById<TextView>(R.id.tv_greeting).text = "Hola, $loggedUserName!"
+
     }
 
     fun Iradress(view: View) {
         val intent = Intent(this, AddAddressActivity::class.java)
         startActivity(intent)
     }
+
+
 
 }
